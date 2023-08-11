21d8f808d0270884da5d521b0f353ae23d79bd03
Rishikesh <rishikeshpanditrp@gmail.com>
2023-08-11 16:03:08 +0530

    adding cubit

---
+++ 02. Cubit/cubit_/lib/screens/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:cubit_/cubit/internet_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: BlocConsumer<InternetCubit, InternetState>(
+            listener: (context, state) {
+              if (state == InternetState.Gained) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text("Internet Connected"),
+                    backgroundColor: Colors.green,
+                  ),
+                );
+              } else if (state == InternetState.Lost) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text("Internet not Connected"),
+                    backgroundColor: Colors.red,
+                  ),
+                );
+              }
+            },
+            builder: (context, state) {
+              if (state == InternetState.Gained) {
+                return const Text('Connected');
+              } else if (state == InternetState.Lost) {
+                return const Text("Not Connected");
+              } else {
+                return const Text('Loading...');
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
